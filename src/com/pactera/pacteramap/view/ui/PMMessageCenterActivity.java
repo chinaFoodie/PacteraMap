ec224309b56e28c03e4ed5d8a553daa9707466b8
Administrator <ly09219@gmail.com>
2015-07-30 09:46:12 +0800

    init

---
+++ src/com/pactera/pacteramap/view/ui/PMMessageCenterActivity.java
@@ -0,0 +1,31 @@
+package com.pactera.pacteramap.view.ui;
+
+import android.os.Bundle;
+
+import com.pactera.pacteramap.view.PMActivity;
+
+/**
+ * 消息中心界面
+ * 
+ * @author ChunfaLee
+ * @create 2015年7月30日09:17:50
+ * @version 1.00
+ *
+ */
+public class PMMessageCenterActivity extends PMActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+}
