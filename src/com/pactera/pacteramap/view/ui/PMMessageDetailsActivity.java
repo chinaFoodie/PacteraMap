b89d95aeeff5bc505e84c4023e2e5774ad05cfe8
Administrator <ly09219@gmail.com>
2015-08-03 08:58:20 +0800

    service

---
+++ src/com/pactera/pacteramap/view/ui/PMMessageDetailsActivity.java
@@ -0,0 +1,31 @@
+package com.pactera.pacteramap.view.ui;
+
+import android.os.Bundle;
+
+import com.pactera.pacteramap.view.PMActivity;
+
+/**
+ * 消息详情
+ * 
+ * @author ChunfaLee
+ * @create 2015年8月2日21:09:57
+ *
+ */
+public class PMMessageDetailsActivity extends PMActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+}
